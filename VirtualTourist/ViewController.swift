3290ee5196286f7a228c9b96d57b674ffbd80fbe
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-06-15 17:49:41 +0200

    feat: create project

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Stefan Jaindl on 15.06.18.
+//  Copyright © 2018 Stefan Jaindl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
